cda83dfe46214cff818dfbd2eaad127923712e8d
anderson.maliszewski <anderson.maliszewski@pmweb.com.br>
2022-01-23 15:56:39 -0300

    Second Commit

--- 1-Terraform/terraform/variables.tf
+++ 1-Terraform/terraform/variables.tf
@@ -1,6 +1,32 @@
-variable "ami_id" {}
-variable "environment" {}
-variable "instance_type" {}
-variable "ssh_cidr" {}
-variable "tags" {}
-variable "vpc_id" {}
+variable "ami_id" {
+    default = "ami-123456"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "instance_type" {
+    default = "i3.micro"
+}
+
+variable "ssh_cidr" {
+    type =  list(string)
+    default = ["10.0.0.0/16"]
+}
+
+variable "tags" {
+    type = map(string)
+    default = {
+        ApplicationID       = "APP1234567"
+        Description         = "Test Instance"
+        Environment         = "DEV"
+        GBL                 = "123456789"
+        Owner               = "MANAGER@test.com"
+        Name                = "US-EAST-DEV-US-TESTAPP"
+    }
+}
+
+variable "vpc_id" {
+    default = "vpc-12345678"
+}
