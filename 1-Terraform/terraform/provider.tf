840d559c2ac24ab4b4601e7c05c184485cf5862d
anderson.maliszewski <anderson.maliszewski@pmweb.com.br>
2022-01-23 16:16:26 -0300

    Added s3 provider

--- 1-Terraform/terraform/provider.tf
+++ 1-Terraform/terraform/provider.tf
@@ -6,5 +6,14 @@ provider "aws" {
   s3_force_path_style         = true
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
+}
 
+terraform {
+  backend "s3" {
+    bucket         = "RDI"
+    key            = "RDI/1-Terraform/terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform_rdi_locks"
+    encrypt        = true
+  }
 }
